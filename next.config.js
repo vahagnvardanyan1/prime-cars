0747106b22235e360031559908387149b5ae1c2f
Gegham Simonyan <gegham.simonyan1@picsart.com>
2026-01-31 13:28:48 +0400

    fix

--- next.config.js
+++ next.config.js
@@ -3,9 +3,11 @@ const withNextIntl = require("next-intl/plugin")("./src/i18n/request.ts");
 
 const nextConfig = {
   images: {
+    domains: ["crheiatdcgohvxngusnj.storage.supabase.co"],
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "coordinated-taco-gvu1ledv.storage.railway.app" },
+      { protocol: "https", hostname: "crheiatdcgohvxngusnj.storage.supabase.co" },
     ],
   },
   reactStrictMode: true,
