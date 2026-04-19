84936a158f2252fca14671c28b6f1d18a0cd64c4
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2026-04-19 13:12:30 +0400

    add google site verification meta tag to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <ThemeInitScript />
+        <meta name="google-site-verification" content="F4kBhWdm_HcUK5bfLAHiWZSPvzqs8eRlLafm81NZHZw" />
       </head>
       <body>{children}</body>
     </html>
